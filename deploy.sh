772d1cba244b4d9301a2d65c6be92c73e418a0c0
Prasun Das <prasunmax@gmail.com>
2020-04-14 01:17:52 +0530

    fixed the deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t prasunmax/multi-client:latest -t prasunmax/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t prasunmax/multi-server:latest -t prasunmax/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t prasunmax/multi-worker:latest -t prasunmax/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t prasunmax/multi-client:latest -t prasunmax/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t prasunmax/multi-server:latest -t prasunmax/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t prasunmax/multi-worker:latest -t prasunmax/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push prasunmax/multi-client:latest
 docker push prasunmax/multi-client:$SHA
